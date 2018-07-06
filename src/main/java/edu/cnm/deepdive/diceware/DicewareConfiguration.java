7cdea7ceea4cb4eb67bf2d74bdb9689b75c6ce9f
iespi7 <ivanespinoza262@gmail.com>
2018-07-06 13:58:45 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/diceware/DicewareConfiguration.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive.diceware;
+
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.ResourceBundle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DicewareConfiguration {
+
+  @Bean
+  public Random getRandom() {
+    return new SecureRandom();
+  }
+
+  @Bean
+  public ResourceBundle getBundle() {
+    return ResourceBundle.getBundle("wordlist");
+  }
+}
